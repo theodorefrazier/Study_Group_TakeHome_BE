f8359f8fa802e3894697dbe0b7185d5df681e48f
TheodoreBFrazier <theo.frazier@icloud.com>
2022-05-06 15:15:52 -0400

    pair programming session: added active to seed file

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,9 +1,9 @@
 \c groups;
 
-INSERT INTO groups (name, city, state, events, is_free) VALUES
-('Porsche 911 Fan Club', 'New York', 'NY', 'Upstate Porsche Race',  false),
-('Men Knit Too', 'Boston', 'MA', 'Sweater knitting class',  true),
-('Front End Developers of East Coast', 'Charlotte', 'NC', 'Learn React!', true),
-('Cat fans 🐈‍⬛', 'South Orange', 'NJ', 'ASPCA volunteering - Cat Visit Day!🐱', true),
-('High Fashion Lovers Club', 'New York', 'NY', 'Architect of Fashion: Gianfranco Ferre Appreciation Panel', true);
+INSERT INTO groups (name, city, state, events, is_free, active) VALUES
+('Porsche 911 Fan Club', 'New York', 'NY', 'Upstate Porsche Race',  false, false),
+('Men Knit Too', 'Boston', 'MA', 'Sweater knitting class',  true, false,),
+('Front End Developers of East Coast', 'Charlotte', 'NC', 'Learn React!', true, true),
+('Cat fans 🐈‍⬛', 'South Orange', 'NJ', 'ASPCA volunteering - Cat Visit Day!🐱', true, true),
+('High Fashion Lovers Club', 'New York', 'NY', 'Architect of Fashion: Gianfranco Ferre Appreciation Panel', true, false);
 
